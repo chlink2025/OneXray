086ee0c9e25f4fd7e257c07945df6f632a3b3df9
mr <you@example.com>
2026-04-22 14:21:00 +0800

    try to remove deeply

--- lib/pages/home/component/ad_row/controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class GoogleAdsRowState {
-  const GoogleAdsRowState();
-}
-
-class GoogleAdsRowController extends Cubit<GoogleAdsRowState> {
-  GoogleAdsRowController() : super(const GoogleAdsRowState());
-}
